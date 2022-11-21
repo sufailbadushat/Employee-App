e4bb53035e805d0f4cafd53cba8650e6988cf83b
sufailbadushat <sufailbadushat@gamil.com>
2022-11-21 16:06:36 +0530

    development

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        int condition;
+        while (true) {
+            System.out.println("-------------------------Operation Menu-----------------------------------");
+            System.out.println("1.Add an employee");
+            System.out.println("2.View all employee");
+            System.out.println("3.Search an employee");
+            System.out.println("4.Delete an employee");
+            System.out.println("5.Exit");
+
+            System.out.println("Enter you operation number: ");
+            condition = sc.nextInt();
+            switch (condition) {
+
+            }
+        }
     }
 }
